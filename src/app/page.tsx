9160dd24674dfdb645f15f85eec7a301921d16a5
pratikmogaveera <pratikmogaveera@gmail.com>
2024-06-22 19:27:15 +0530

    add scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Banner from "@/components/Banner"
 import Header from "@/components/Header"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 export default function Home() {
     return (
-        <main className="min-h-screen w-full justify-items-center bg-background text-foreground">
-            <Header />
-            <Banner />
-        </main>
+        <ScrollArea className="h-svh">
+            <main className="w-full justify-items-center bg-background text-foreground">
+                <Header />
+                <Banner />
+            </main>
+        </ScrollArea>
     )
 }
