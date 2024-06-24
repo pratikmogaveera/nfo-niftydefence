9183613f4ab6292fcda32b4598efc31e81419bcc
pratikmogaveera <pratik.mogaveera@we3.tech>
2024-06-24 11:09:50 +0530

    updated Wrapper

--- src/components/helpers/Wrapper.tsx
+++ src/components/helpers/Wrapper.tsx
@@ -1,7 +1,18 @@
+import { cn } from "@/lib/utils"
 import React from "react"
 
-const Wrapper = ({ children }: { children: React.ReactNode }) => {
-    return <section className="w-full max-w-[1366px]">{children}</section>
+const Wrapper = ({
+    children,
+    className,
+}: {
+    children: React.ReactNode
+    className?: string
+}) => {
+    return (
+        <section className={cn("w-full max-w-[1366px]", className)}>
+            {children}
+        </section>
+    )
 }
 
 export default Wrapper
